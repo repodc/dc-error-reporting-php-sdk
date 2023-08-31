44bc9d6ac6eb6c1875c0826d0316ae1bc5ac6b83
VinyVidal <vinicius.vidalp@hotmail.com>
2023-08-31 15:55:16 -0300

    Adicionando token ao criar a request

--- src/DcErrorReportingSdk.php
+++ src/DcErrorReportingSdk.php
@@ -9,35 +9,46 @@ class DcErrorReportingSdk {
 
     private string $systemName;
     private string $enviroment;
+    private $token;
 
-    public function __construct(string $systemName, string $enviroment) {
+    public function __construct(string $systemName, string $enviroment, $token) {
         $this->systemName = $systemName;
         $this->enviroment = $enviroment;
+        $this->token = $token;
     }
 
     public function send(Throwable $th) {
-        $data = [
-            'system_name' => $this->systemName,
-            'environment' => $this->enviroment,
-            'error' => $th->__toString(),
-        ];
-        
-        $ch = curl_init($this->apiUrl . '/api/error_report');
-        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-        curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'POST');
-        curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($data));
-        curl_setopt($ch, CURLOPT_HTTPHEADER, [
-            'Content-Type: application/json',
-        ]);
-        
-        $response = curl_exec($ch);
-        
-        if (curl_errno($ch)) {
-            echo 'Curl error: ' . curl_error($ch);
-        }
-        
-        curl_close($ch);
+        try {
+            if (!$this->token || (!$this->enviroment || $this->enviroment === 'local')) {
+                return;
+            }
 
-        return $response;
+            $data = [
+                'system_name' => $this->systemName,
+                'environment' => $this->enviroment,
+                'error' => $th->__toString(),
+            ];
+
+            $ch = curl_init($this->apiUrl . '/api/error_report');
+            curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+            curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'POST');
+            curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($data));
+            curl_setopt($ch, CURLOPT_HTTPHEADER, [
+                'Content-Type: application/json',
+                'Authorization: Bearer ' . $this->token
+            ]);
+
+            $response = curl_exec($ch);
+
+            if (curl_errno($ch)) {
+                echo 'Curl error: ' . curl_error($ch);
+            }
+
+            curl_close($ch);
+
+            return $response;
+        } catch (\Throwable $th) {
+            // Se der erro nada vai ser feito
+        }
     }
 }
