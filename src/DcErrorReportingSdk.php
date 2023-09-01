e7a3ede966b2a5ffeab7060440d7566ef56ead59
VinyVidal <vinicius.vidalp@hotmail.com>
2023-09-01 13:27:37 -0300

    Correcao environment

--- src/DcErrorReportingSdk.php
+++ src/DcErrorReportingSdk.php
@@ -8,24 +8,24 @@ class DcErrorReportingSdk {
     private string $apiUrl = 'https://dc-error-reporting.dctec.dev';
 
     private string $systemName;
-    private string $enviroment;
+    private string $environment;
     private $token;
 
-    public function __construct(string $systemName, string $enviroment, $token) {
+    public function __construct(string $systemName, string $environment, $token) {
         $this->systemName = $systemName;
-        $this->enviroment = $enviroment;
+        $this->environment = $environment;
         $this->token = $token;
     }
 
     public function send(Throwable $th) {
         try {
-            if (!$this->token || (!$this->enviroment || $this->enviroment === 'local')) {
+            if (!$this->token || (!$this->environment || $this->environment === 'local')) {
                 return;
             }
 
             $data = [
                 'system_name' => $this->systemName,
-                'environment' => $this->enviroment,
+                'environment' => $this->environment,
                 'requested_url' => $this->getServerRequestedUrl(),
                 'error' => $th->__toString(),
             ];
